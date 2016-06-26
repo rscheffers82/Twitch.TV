5151c637db06ff9e345ba711a701b958cba19c87
Roy Scheffers <r.scheffers@gmail.com>
2016-06-25 21:25:09 -0400

    Update display result function as it now uses div .table-row, table-cell structure to display results Minor code clean-ups

--- main_script.js
+++ main_script.js
@@ -3,20 +3,38 @@ $( document ).ready(function() {
   getResults (channels);
 });
 
-
 /*
 Usage details see the below link: 
 https://github.com/justintv/Twitch-API/blob/master/v3_resources/streams.md#get-streamschannel
 */
-var channels = ["blackbeltproofreader", "brunofin", "nalcs1", "OgamingSC2", "storbeck", "comster404", "cretetion", "ESL_SC2", "habathcx", "RobotCaleb", "noobs2ninjas", "dawah200", "esportsarena", "summit1g", "nl_kripp"];
 var url = "https://api.twitch.tv/kraken/";
-stuff = [];
+var channels = ["blackbeltproofreader",
+               "brunofin",
+               "nalcs1",
+               "OgamingSC2",
+               "storbeck",
+               "comster404",
+               "cretetion",
+               "ESL_SC2",
+               "habathcx",
+               "RobotCaleb",
+               "noobs2ninjas",
+               "dawah200",
+               "esportsarena",
+               "summit1g",
+               "nl_kripp"];
+// CodePen usage
+//var $NO_IMG = 'https://github.com/rscheffers82/TwitchTV/raw/master/images/no-channel.jpg';
+// GitHub / local usage
+ var $NO_IMG = 'images/no-channel.jpg';
 
 var getResults = function (twData) {
   // check if the array is still containing elements
   if ( twData.length < 1 ) return;
+  
   // take the first element of the array 
   var currentChannel = twData.shift();
+  
   // API call to get the twitch channel info
   $.getJSON(url + "channels/" + encodeURI(currentChannel))
     .done(function(data, textStatus, jqXHR) {
@@ -24,6 +42,7 @@ var getResults = function (twData) {
       info.logo = data.logo
       info.name = data.display_name;
       info.url = data.url;
+      
       // 2nd API call to check if the channel is streaming content
       $.getJSON(url + "streams/" + encodeURI(currentChannel))
         .done(function(data){
@@ -39,12 +58,13 @@ var getResults = function (twData) {
           getResults(twData);
         });
   })
+  
   .fail(function(jqXHR, textStatus, errorThrown) {
     // account closed or not found error handling
     //console.log('failed to find the name' + errorThrown.toString());
     //console.log(jqXHR);
     var info = {};
-    info.logo = 'images/no-channel.jpg';
+    info.logo = $NO_IMG
     info.name = jqXHR.responseJSON.message.split('\'')[1];
     info.status = 'Account Closed';
     info.url = '#';
@@ -58,15 +78,12 @@ var getResults = function (twData) {
 var display = function(info){
   // format the data in a html table
   var html = '';
-  if (info.online) {
-    html += '<tr class="' + info.name.toLowerCase() + ' online">';
-  } else {
-    html += '<tr class="' + info.name.toLowerCase() + ' offline">';
-  }
-  html += '<td style="width:50px;"><img class="ico" src="' + info.logo + '"></td>';
-  html += '<td style="width:50px;"><a href="' + info.url + '" target="_blank">' + info.name + '</td>';
-  html += '<td class="center ' + info.name.toLowerCase() + '">' + info.status + '</td>';
-  html += '</tr>';
-  if (info.online) $('#results').prepend( html );
-  else $('#results').append( html );
+  
+    html += '<div class="channel">';
+      html += '<div class="logo ' + (info.online ? 'online' : 'offline') + '"><img class="ico" src="' + info.logo + '"></div>';
+      html += '<div class="name ' + (info.online ? 'online' : 'offline') + '"><a href="' + info.url + '" target="_blank">' + info.name + '</div>';
+      html += '<div class="status ' + (info.online ? 'online' : 'offline') + '">' + info.status + '</div>';
+    html += '</div>';
+
+  info.online ? $('#results').prepend( html ) : $('#results').append( html );
 }
